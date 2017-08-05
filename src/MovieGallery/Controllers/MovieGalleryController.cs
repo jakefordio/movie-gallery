fe2fd6b0f7b247bc22634a53d1a1e6148106b2d0
Jake Ford <jake@jakeford.io>
2017-08-05 11:38:58 -0500

    Added action method Detail()

--- src/MovieGallery/Controllers/MovieGalleryController.cs
+++ src/MovieGallery/Controllers/MovieGalleryController.cs
@@ -5,8 +5,12 @@ using System.Web;
 using System.Web.Mvc;
 
 namespace MovieGallery.Controllers
-{
+{   //Controller classes need to be public for MVC to use them!
     public class MovieGalleryController : Controller
-    {
+    {   //Action methods need to be public for MVC to find and use them!
+        public string Detail()
+        {
+            return "Welcome to the mother fucking movie gallery bitch.";
+        }
     }
 }
