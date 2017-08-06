5dce5324e74361e3446534e062974aa56b540926
Jake Ford <jake@jakeford.io>
2017-08-06 16:24:31 -0500

    Added Design Files

--- src/MovieGallery/Controllers/MovieGalleryController.cs
+++ src/MovieGallery/Controllers/MovieGalleryController.cs
@@ -10,6 +10,18 @@ namespace MovieGallery.Controllers
     {   //Action methods need to be public for MVC to find and use them!
         public ActionResult Detail()
         {
+            ViewBag.Title = "Rogue One: A Star Wars Trilogy";
+            ViewBag.Rated = "PG-13";
+            ViewBag.Length = "2h 13m";
+            ViewBag.Plot = "<p><b>Plot</b>: In a time of conflict, a group of unlikely heroes band together on a mission to steal the plans to the Death Star, the Empire’s ultimate weapon of destruction. This key event in the Star Wars timeline brings together ordinary people who choose to do extraordinary things, and in doing so, become part of something greater than themselves.</p>";
+            ViewBag.Actors = new string[]
+            {
+                "Felicity Jones",
+                "Diego Luna",
+                "Alan Tudyk",
+                "Donnie Yen",
+                "Wen Jiang"
+            };
             return View();
         }
     }
