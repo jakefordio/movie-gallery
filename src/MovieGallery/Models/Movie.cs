ad927771f14f0498db06b2755c589670d2e064e2
Jake Ford <jake@jakeford.io>
2017-08-06 22:36:30 -0500

    Added Movie Model

---
+++ src/MovieGallery/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieGallery.Models
+{
+    public class Movie
+    {
+        string Id { get; set; }
+        string Title { get; set; }
+        string Rated { get; set; }
+        string Length { get; set; }
+        string PlotHtml { get; set; }
+        string[] Actors { get; set; }
+    }
+}
